96662d1a7ca6614acd1dee6df7dc7f4ccff16d9d
brunoJorquera <jorquera.b18@gmail.com>
2021-09-17 10:33:30 -0700

    First Save

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+  // choose random complimentlet 
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+app.get('/api/fortune', (req,res) => {
+  const fortunes = ["A bright path awaits you in your future!",
+  "Your zipper is down, might want to fix that.",
+  "Remember to floss, you might need to, especially soon!",
+  "Beds don't make themselves, seek organization in your life.",
+  "Look both ways before crossing the street, it might save you!"
+];
+
+let randomIndex = Math.floor(Math.random() * fortunes.length);
+let randomFortune = fortunes[randomIndex]
+
+res.status(200).send(randomFortune)
+
+});
+
+app.listen(4000, () => console.log("Server running on 4000"));
